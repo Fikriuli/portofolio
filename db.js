751988c4fe73bbf8a0ec9fb5d5b41434bd54e7da
Fikriuli <43263530+Fikriuli@users.noreply.github.com>
2019-05-14 09:39:01 +0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+[
+  {
+    "judul"     : "Donal",
+    "thumnile"  : "https://www.axios.com",
+    "deskripsi" : "Belajar Axios"
+  },
+  
+  {
+    "judul"     : "Donil",
+    "thumnile"  : "https://www.axios.com",
+    "deskripsi" : "Belajar Axios"
+  },
+  
+  {
+    "judul"     : "Donul",
+    "thumnile"  : "https://www.axios.com",
+    "deskripsi" : "Belajar Axios"
+  }
+]
